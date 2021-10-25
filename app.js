c10c042c354d1baf0eee4bfc5d832cd0faa660f2
egg <egg810207@gmail.com>
2021-10-25 17:37:58 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+// 讀取 JSON 檔案，將種子資料載入應用程式
+const restaurantList = require('./restaurant.json')
+
+// 把資料帶入 handlebars 樣板中動態呈現
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  // 操作 handlebars 中的 each 迴圈呈現出多張餐廳卡片
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// 應用 params 打造動態路由
+app.get('/restaurants/:restaurants_id', (req, res) => {
+  const id = Number(req.params.restaurants_id)
+  const restaurant = restaurantList.results.find((restaurant) => {
+    return restaurant.id === id
+  })
+  res.render('show', { restaurant })
+})
+
+// 用 Query String 打造 search 功能
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return (restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase()))
+  })
+  res.render('index', { restaurants, keyword: req.query.keyword })
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
